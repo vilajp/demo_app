05ac601635e2eb7da7b27ec20e24070a8efc25d5
vilajp <vilajp@gmail.com>
2011-02-05 23:35:37 -0300

    Listo con la Aplicacion demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
   resources :users
 
   # The priority is based upon order of creation:
@@ -7,8 +9,7 @@ DemoApp::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
+   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
@@ -51,7 +52,7 @@ DemoApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-
+ root :to => "users#index"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
